bbb0fbfe2df4a3960f3e991c010f0963dd3302cb
Joe Sutton <hungryed6@gmail.com>
2015-05-13 23:54:39 -0700

    poor mans query string translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_locale
+
+  def set_locale
+    @locale = params.fetch('t', :en).to_sym
+    unless [:en, :sp].include?(@locale)
+      @locale = :en
+    end
+    I18n.locale = @locale
+  end
 end
